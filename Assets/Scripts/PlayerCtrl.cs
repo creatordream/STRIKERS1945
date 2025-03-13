6c4e6bb17d803d846b865324fd7daf11c2a19921
creatordream <ctwwg2000@gmail.com>
2025-03-13 16:27:58 +0900

    리소스 정리
    중복 리소스 제거 및 bmp파일 png로 변경(70% 진행)

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    //이동, 공격, 필살기, 호위함 집중공격?, 생명력(부활횟수), 
+    public int speed = 3; // 이동 속도
+    public float damage = 5f; //기본 데미지
+    public float guardDamage = 3f; //호위함 데미지
+    public float lethalDamage = 50f; //필살기 데미지
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         
     }
@@ -11,6 +19,9 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxisRaw("Horizontal"); //좌우
+        float v = Input.GetAxisRaw("Vertical"); //상하
+
         
     }
 }
--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class TitleManager : MonoBehaviour
 {
-    //점수, 부순 적 수, (추가 확장? : 업적, 업그레이드, 난이도, 신규캐릭 해금)
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
